ee72bb2a372417a875da08e7a6c6ae07276adfea
Kavya982 <medakavya0982@gmail.com>
2025-04-03 22:28:51 +0530

    Add files via upload

---
+++ setAcessing.py
@@ -0,0 +1,8 @@
+print(set("HellowelcomeHello"))
+print(set(["hello","for","hello","for"]))
+print(type(set()))
+st = {1,4,5,67}
+st.add(23)
+print(st)
+ft = frozenset({1,2,3,4,5,6,7,8,1,2,3,45})
+print(ft)
