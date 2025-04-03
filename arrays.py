2bd272dfe4977d24d6edd04527dc9db725c113f8
Kavya982 <medakavya0982@gmail.com>
2025-04-03 22:28:03 +0530

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,10 @@
+import array 
+nums = array.array("i",(123,24,36,472,58))
+print(len(nums))
+print(sorted(nums))
+for i in nums:
+    print(i)
+
+# fruits = array.array('u',"apple,banana,orange")
+# for i in fruits:
+#     print(i)
