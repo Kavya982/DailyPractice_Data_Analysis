bee4aa36d1cc42df2ebc5feeee1c77af1aea7aa9
Kavya982 <medakavya0982@gmail.com>
2025-04-04 16:52:26 +0530

    Add files via upload

---
+++ stringformat.py
@@ -0,0 +1,9 @@
+name="keifer"
+age = 23
+print("I am {} and i am an actor. I am {} years old".format(name,age))
+
+
+print("I am {1} years old and my name is {0}".format(name,age))
+
+
+print(f"I am {name} and i am form philipine of {age} years old")
